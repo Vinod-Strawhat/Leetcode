8f0ed72a0232b835bd0a3e938b89a734085c894c
Vinod-Strawhat <vinodpubg2528@gmail.com>
2026-07-24 07:31:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (16.87%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+         stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(!st.empty()){
+            if(s[i]==')' && st.top()=='('){
+                st.pop();
+            }
+            else if(s[i]==']' && st.top()=='['){
+                st.pop();
+            }
+            else if(s[i]=='}' && st.top()=='{'){
+                st.pop();
+            }
+            else{
+            st.push(s[i]);
+            }}
+            else{
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
