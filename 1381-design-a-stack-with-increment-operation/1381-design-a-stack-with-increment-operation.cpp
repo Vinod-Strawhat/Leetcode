8b78f56535fcb8a980764b2e88fa84036da91b0c
Vinod-Strawhat <vinodpubg2528@gmail.com>
2026-07-25 14:32:02 +0530

    Time: 4 ms (59.42%), Space: 29 MB (44.31%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,43 @@
+class CustomStack {
+public:
+    int maxSize;
+    vector<int> st;
+    vector<int> inc;
+
+    CustomStack(int maxSize) {
+        this->maxSize = maxSize;
+    }
+
+    void push(int x) {
+        if (st.size() == maxSize)
+            return;
+
+        st.push_back(x);
+        inc.push_back(0);
+    }
+
+    int pop() {
+        if (st.empty())
+            return -1;
+
+        int i = st.size() - 1;
+
+        if (i > 0)
+            inc[i - 1] += inc[i];
+
+        int ans = st.back() + inc[i];
+
+        st.pop_back();
+        inc.pop_back();
+
+        return ans;
+    }
+
+    void increment(int k, int val) {
+        if (st.empty())
+            return;
+
+        int i = min(k, (int)st.size()) - 1;
+        inc[i] += val;
+    }
+};
