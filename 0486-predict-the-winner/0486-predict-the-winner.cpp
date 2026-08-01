447f16a010c5c223477a6b36d2e463f969254ebf
Vinod-Strawhat <vinodpubg2528@gmail.com>
2026-08-01 06:10:40 +0530

    Time: 3 ms (39.21%), Space: 9.5 MB (96.48%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool playgame(int turn, int s1, int s2, int i, int j, vector<int>& nums) {
+        // base case
+        if (i > j) {
+            return s1 >= s2;
+        }
+
+        if (turn) {
+            // for player 1 to win, he should win in atleast one future
+            return playgame(0, s1 + nums[i], s2, i + 1, j, nums) 
+                || playgame(0, s1 + nums[j], s2, i, j - 1, nums);
+        }
+        // no matter what player 2 chooses, player 1 has to win, so thats why &&
+        return playgame(1, s1, s2 + nums[i], i + 1, j, nums) 
+            && playgame(1, s1, s2 + nums[j], i, j - 1, nums);
+
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        return playgame(1, 0, 0, 0, nums.size() - 1, nums);
+    }
+};
