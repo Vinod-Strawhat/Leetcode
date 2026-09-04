d78edb5f51ddbccdda329e2ce73267bac4675776
R Vinod <vinodpubg2528@gmail.com>
2026-09-04 05:33:35 +0530

    Time: 28 ms (14.59%), Space: 12.2 MB (40.33%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    set<vector<int>> s;
+    void per(vector<int>& nums,int idx,vector<vector<int>> &ans){
+        if(idx==nums.size()){
+            if(s.find(nums)==s.end()){
+            ans.push_back(nums);
+            s.insert(nums);
+            }
+            return;
+        }
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[idx],nums[i]);
+            per(nums,idx+1,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        per(nums,0,ans);
+        return ans;
+    }
+};
